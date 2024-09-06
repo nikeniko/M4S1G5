fcac9a46615e29549b44deda9919b164c484f09a
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-09-06 13:19:12 +0200

    Initial commit

---
+++ src/main/java/nicolas/demo/M4S1G5Application.java
@@ -0,0 +1,13 @@
+package nicolas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M4S1G5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M4S1G5Application.class, args);
+	}
+
+}
---
+++ src/test/java/nicolas/demo/M4S1G5ApplicationTests.java
@@ -0,0 +1,13 @@
+package nicolas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M4S1G5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
